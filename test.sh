36407ac31d95db6331b0cb4edc2d964cf9cbe1af
Richard Eliáš <richard.elias@matfyz.cz>
2015-08-04 12:23:57 +0200

    opraveny test..

--- test.sh
+++ test.sh
@@ -9,26 +9,10 @@ generate_crontab() {
 	echo "$min * * * * yes > /dev/null"
 	echo "$min1 * * * * killall yes"
 	) >> crontab
+
 }
 run() {
 	make run
 }
-kill_all() {
-	echo kill_all $$
-	ps -e -o ppid= -o pid= | grep "$$ " 
-	ps -e -o ppid= -o pid=
-	ps -e -o ppid= -o pid= | grep "$$ " |
-	while read pid pid
-	do
-		echo kill $pid
-		kill $pid
-	done
-}
-terminate() {
-	TIME=2
-	sleep $TIME && kill_all &
-}
-
 generate_crontab
-run&
-terminate
+run
